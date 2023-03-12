9b9996e01f437dc72a499e1e336f54241169bb00
jkingportfolio <mycodejourney3@gmail.com>
2023-03-12 18:24:50 +0000

    Add: Task app and model

---
+++ tasks/models.py
@@ -0,0 +1,40 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.db import models
+from django.contrib.auth.models import User
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class Task(models.Model):
+    """
+    A class for the task model
+    """
+    PRIORITY_CHOICES = [
+        ('HIGH', 'High'),
+        ('MEDIUM', 'Medium'),
+        ('LOW', 'Low'),
+    ]
+
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255, blank=True)
+    task_body = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=50, blank=True)
+    title = models.CharField(max_length=255, blank=True)
+    priority = models.CharField(choices=PRIORITY_CHOICES, default='LOW')
+    due_date = models.DateTimeField()
+    files = models.FileField(blank=True)
+    assigned_to = models.ForeignKey(User,
+                                    on_delete=SET_NULL,
+                                    related_name='assigned_to')
+
+    class Meta:
+        ordering = ['due_date']
+
+    def __str__(self):
+        """
+        Return information of the Task
+        """
+        return f"Task: {self.id}"
