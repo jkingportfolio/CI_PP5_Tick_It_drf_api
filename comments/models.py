b340f9573f2ca489a959ad10d0a5cb5a06e6d18f
jkingportfolio <mycodejourney3@gmail.com>
2023-03-13 20:13:14 +0000

    Add: Comment model to comments app

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,27 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.db import models
+from django.contrib.auth.models import User
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Create your models here.
+# Internal:
+from posts.models import Task
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class Comment(models.Model):
+    """
+    A class for the comment model
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.content
