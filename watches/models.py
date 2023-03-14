095bba83da02b8abf7f09cd81081ac79ff68bcbf
jkingportfolio <mycodejourney3@gmail.com>
2023-03-14 18:51:02 +0000

    Add: Watch model to watches app

--- watches/models.py
+++ watches/models.py
@@ -1,3 +1,33 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.db import models
+from django.contrib.auth.models import User
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Create your models here.
+# Internal:
+from tasks.models import Task
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class Watch(models.Model):
+    """
+    A class for the watch model
+    Related to 'owner' and 'tasks'
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(
+        Task,
+        on_delete=models.CASCADE,
+        related_name='watches'
+        )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # 'unique_together' prevents user from watching the same task twice
+        # https://docs.djangoproject.com/en/4.1/ref/models/options/#unique-together
+        unique_together = ['owner', 'task']
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f'{self.owner} is watching task #{self.task.id}.'
