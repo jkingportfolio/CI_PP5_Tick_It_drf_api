c24da8455219a8936b92e8216e2dc4de39a60cb7
jkingportfolio <mycodejourney3@gmail.com>
2023-03-14 19:45:03 +0000

    Add: Watches app classes to view.py

--- watches/views.py
+++ watches/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from rest_framework import generics, permissions
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Create your views here.
+# Internal:
+from .models import Watch
+from .serializers import WatchSerializer
+from tick_it_drf_api.permissions import IsOwnerOrReadOnly
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class WatchList(generics.ListCreateAPIView):
+    """
+    A class for WatchList
+    """
+    serializer_class = WatchSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+        ]
+    queryset = Watch.objects.all()
+
+    def perform_create(self, serializer):
+        # owner is the user making the request
+        serializer.save(owner=self.request.user)
+
+
+class WatchDetail(generics.RetrieveDestroyAPIView):
+    """
+    A class for WatchDetail
+    User to be able to retrieve and delete their watch
+    of a task
+    """
+    serializer_class = WatchSerializer
+    permission_classes = [
+        IsOwnerOrReadOnly
+        ]
+    queryset = Watch.objects.all()
