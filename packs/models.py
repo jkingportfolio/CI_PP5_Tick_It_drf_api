71ce72978377326b47a9da6be02a25c0c88d5f8d
jkingportfolio <mycodejourney3@gmail.com>
2023-03-15 20:41:43 +0000

    Add: Pack class to models.py

--- packs/models.py
+++ packs/models.py
@@ -1,3 +1,27 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.db import models
+from django.contrib.auth.models import User
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Create your models here.
+
+class Pack(models.Model):
+    """
+    A class for the pack model
+    """
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255, blank=False)
+    pack_description = models.TextField(blank=True)
+    members = models.ManyToManyField(User, related_name='members')
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        """
+        Return information of the Pack
+        """
+        return f"Pack: #{self.title}"
