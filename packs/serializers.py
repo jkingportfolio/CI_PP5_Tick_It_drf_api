0e4bb7862e87fb524cdf75b0468d9c9e83c6a2fd
jkingportfolio <mycodejourney3@gmail.com>
2023-03-15 20:53:44 +0000

    Add: PackSerializer to packs app

--- packs/serializers.py
+++ packs/serializers.py
@@ -1,1 +1,44 @@
- 
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from rest_framework import serializers
+
+# Internal:
+from .models import Pack
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class PackSerializer(serializers.ModelSerializer):
+    """
+    A class for a TaskSerializer
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Pack
+        fields = [
+            'id',
+            'is_owner',
+            'owner',
+            'created_on',
+            'title',
+            'pack_description',
+            'members',
+            'updated_on',
+        ]
+
+    def create(self, validated_data):
+        """
+        Handle possible duplication
+        """
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplication'
+            })
