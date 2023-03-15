de49cc0ead3fa9cb0e3c086bc3530a50c29ac14c
jkingportfolio <mycodejourney3@gmail.com>
2023-03-15 21:22:44 +0000

    Add: PackList to views.py and update ownerfield

--- packs/models.py
+++ packs/models.py
@@ -10,7 +10,7 @@ class Pack(models.Model):
     """
     A class for the pack model
     """
-    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255, blank=False)
     pack_description = models.TextField(blank=True)
--- packs/views.py
+++ packs/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from rest_framework import generics, permissions
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Create your views here.
+# Internal:
+from .models import Pack
+from .serializers import PackSerializer
+from tick_it_drf_api.permissions import IsOwnerOrReadOnly
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class PackList(generics.ListCreateAPIView):
+    """
+    A class for PackList
+    """
+    serializer_class = PackSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+        ]
+    queryset = Pack.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
