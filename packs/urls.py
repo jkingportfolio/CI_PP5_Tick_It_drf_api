13d45e969f60d34d7d61c82addde1a7c28339d15
jkingportfolio <mycodejourney3@gmail.com>
2023-03-15 21:53:35 +0000

    Add: PackList path to packs urls.py

--- packs/urls.py
+++ packs/urls.py
@@ -1,1 +1,13 @@
- 
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.urls import path
+
+# Internal:
+from packs import views
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+urlpatterns = [
+    path('packs/', views.PackList.as_view()),
+]
