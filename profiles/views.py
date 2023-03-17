bd93d1469677324cd122c8bac85a113568b755f5
jkingportfolio <mycodejourney3@gmail.com>
2023-03-17 22:40:58 +0000

    Refact: ProfileDetail to use generics

--- profiles/views.py
+++ profiles/views.py
@@ -39,38 +39,40 @@ class ProfileList(generics.ListAPIView):
         'watching_count',
     ]
 
-    # def get(self, request):
-    #     profiles = Profile.objects.all()
-    #     serializer = ProfileSerializer(profiles,
-    #                                    many=True,
-    #                                    context={'request': request})
-    #     return Response(serializer.data)
 
-
-class ProfileDetail(APIView):
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
 
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
+    queryset = Profile.objects.annotate(
+        tasks_count=Count(
+            'owner__task',
+            distinct=True),
+        watching_count=Count(
+            'owner__watch',
+            distinct=True)
+    ).order_by('-created_on')
 
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile,
-                                       context={'request': request})
-        return Response(serializer.data)
+    # def get_object(self, pk):
+    #     try:
+    #         profile = Profile.objects.get(pk=pk)
+    #         self.check_object_permissions(self.request, profile)
+    #         return profile
+    #     except Profile.DoesNotExist:
+    #         raise Http404
 
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile,
-                                       data=request.data,
-                                       context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def get(self, request, pk):
+    #     profile = self.get_object(pk)
+    #     serializer = ProfileSerializer(profile,
+    #                                    context={'request': request})
+    #     return Response(serializer.data)
+
+    # def put(self, request, pk):
+    #     profile = self.get_object(pk)
+    #     serializer = ProfileSerializer(profile,
+    #                                    data=request.data,
+    #                                    context={'request': request})
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
