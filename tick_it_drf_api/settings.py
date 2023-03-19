dc1efc73602a8335db5123eae9ff94eb366c0fec
jkingportfolio <mycodejourney3@gmail.com>
2023-03-19 14:12:33 +0000

    Add: Install Django allauth for registration

--- tick_it_drf_api/settings.py
+++ tick_it_drf_api/settings.py
@@ -53,6 +53,11 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework.authtoken',
     'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
     'django.contrib.humanize',
     'profiles',
     'tasks',
@@ -60,7 +65,7 @@ INSTALLED_APPS = [
     'watches',
     'packs',
 ]
-
+SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
