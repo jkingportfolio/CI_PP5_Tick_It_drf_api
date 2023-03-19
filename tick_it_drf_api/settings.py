74d4ff6824698440ec1c05397504f7dec0554a66
jkingportfolio <mycodejourney3@gmail.com>
2023-03-19 18:49:21 +0000

    Update: Missing comma installed apps

--- tick_it_drf_api/settings.py
+++ tick_it_drf_api/settings.py
@@ -84,7 +84,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
-    'corsheaders'
+    'corsheaders',
     'django.contrib.humanize',
     'profiles',
     'tasks',
