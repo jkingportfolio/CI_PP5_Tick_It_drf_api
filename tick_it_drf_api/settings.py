7911e45eee2d78498479b9551b5f625d31244faf
jkingportfolio <mycodejourney3@gmail.com>
2023-03-19 14:04:47 +0000

    Add: Django auth token and rest auth

--- tick_it_drf_api/settings.py
+++ tick_it_drf_api/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework',
     'django_filters',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
     'django.contrib.humanize',
     'profiles',
     'tasks',
