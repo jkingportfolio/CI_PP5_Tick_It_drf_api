7e02fa11a4ed41df0287d1fc80c4dd50fb280460
jkingportfolio <mycodejourney3@gmail.com>
2023-03-27 08:56:40 +0000

    Update: api-auth url error

--- tick_it_drf_api/urls.py
+++ tick_it_drf_api/urls.py
@@ -20,7 +20,7 @@ from .views import home, logout_route
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
-    path('api-auth', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/',
