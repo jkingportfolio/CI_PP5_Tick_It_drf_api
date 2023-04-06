5518f68d32c348ea0eda2cb7ced523e42383f40f
jkingportfolio <mycodejourney3@gmail.com>
2023-04-06 10:15:18 +0000

    Update: Complete field data type

--- tasks/models.py
+++ tasks/models.py
@@ -19,6 +19,12 @@ class Task(models.Model):
         ('LOW', 'Low'),
     ]
 
+    COMPLETION_STATUS = [
+        ('NO', 'No'),
+        ('IN-PROGRESS', 'In-progress'),
+        ('COMPLETE', 'Complete'),
+    ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255, blank=True)
@@ -28,13 +34,16 @@ class Task(models.Model):
     priority = models.CharField(max_length=255,
                                 choices=PRIORITY_CHOICES,
                                 default='LOW')
-    due_date = models.DateField()
+    due_date = models.DateField(blank=True, null=True)
     files = models.FileField(blank=True)
     assigned_to = models.ForeignKey(User,
                                     null=True,
                                     on_delete=models.SET_NULL,
                                     related_name='assigned_to')
-    completed = models.BooleanField(default=False, blank=True, null=True)
+    # completed = models.BooleanField(default=False, blank=True, null=True)
+    completed = models.CharField(max_length=255,
+                                 choices=COMPLETION_STATUS,
+                                 default='NO')
     pack = models.ForeignKey(Pack, on_delete=models.CASCADE, blank=True,
                              null=True)
 
