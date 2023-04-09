ed455cba3ee593d930f36ae6dc99b56027c1ee08
jkingportfolio <mycodejourney3@gmail.com>
2023-04-09 12:14:44 +0000

    Update: DATETIME_FORMAT

--- tick_it_drf_api/settings.py
+++ tick_it_drf_api/settings.py
@@ -206,6 +206,8 @@ USE_TZ = True
 
 DATE_INPUT_FORMATS = ["%d/%m/%Y"]
 
+DATETIME_FORMAT: ['%d %m %Y']
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
