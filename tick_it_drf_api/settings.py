772f5a8983766d608b1e87877fd294c260668657
jkingportfolio <mycodejourney3@gmail.com>
2023-04-10 20:02:09 +0000

    Update: DEBUG value in settings.py

--- tick_it_drf_api/settings.py
+++ tick_it_drf_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
