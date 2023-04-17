9d45e0666d7d22c525df8afc88481096ecaf6b7f
jkingportfolio <mycodejourney3@gmail.com>
2023-04-17 13:37:15 +0000

    Update: Pack serializers task serializer

--- packs/serializers.py
+++ packs/serializers.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 
 # Internal:
 from .models import Pack
+from tasks.serializers import TaskSerializer
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -16,7 +17,7 @@ class PackSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    tasks = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    tasks = TaskSerializer(many=True)
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -38,15 +39,13 @@ class PackSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
+     
         tasks_data = validated_data.pop('tasks', [])
         pack = Pack.objects.create(**validated_data)
 
         for task_id in tasks_data:
             task = Task.objects.get(id=task_id)
-            pack.tasks.add(task)
-
-        return pack
-
+            pack.tasks.create(**validated_data)
 
         return pack
 
