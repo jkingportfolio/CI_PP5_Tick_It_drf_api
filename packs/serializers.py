7d23f7aad76eaf2af0fc105da71faf586014d666
jkingportfolio <mycodejourney3@gmail.com>
2023-04-17 13:13:39 +0000

    Test: Pack app updates for api

--- packs/serializers.py
+++ packs/serializers.py
@@ -5,17 +5,54 @@ from rest_framework import serializers
 
 # Internal:
 from .models import Pack
+from tasks.serializers import TaskSerializer
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
+# class PackSerializer(serializers.ModelSerializer):
+#     """
+#     A class for a PackSerializer
+#     """
+#     owner = serializers.ReadOnlyField(source='owner.username')
+#     is_owner = serializers.SerializerMethodField()
+#     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+#     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+#     def get_is_owner(self, obj):
+#         request = self.context['request']
+#         return request.user == obj.owner
+
+#     class Meta:
+#         model = Pack
+#         fields = [
+#             'id',
+#             'is_owner',
+#             'owner',
+#             'created_on',
+#             'title',
+#             'pack_description',
+#             'updated_on',
+#             'tasks',
+#             'profile_id',
+#             'profile_image',
+#         ]
+
+#     def create(self, validated_data):
+#         tasks_data = validated_data.pop('tasks')
+#         pack = Pack.objects.create(**validated_data)
+
+#         for task_data in tasks_data:
+#             task, _ = Task.objects.get(id=task_data['id'])
+#             pack.tasks.add(task)
+
+#         return pack
+
 class PackSerializer(serializers.ModelSerializer):
-    """
-    A class for a PackSerializer
-    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    tasks = TaskSerializer(many=True, required=False)
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -37,11 +74,11 @@ class PackSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
-        tasks_data = validated_data.pop('tasks')
+        tasks_data = validated_data.pop('tasks', [])
         pack = Pack.objects.create(**validated_data)
 
         for task_data in tasks_data:
-            task, _ = Task.objects.get(id=task_data['id'])
+            task = Task.objects.create(**task_data)
             pack.tasks.add(task)
 
         return pack
