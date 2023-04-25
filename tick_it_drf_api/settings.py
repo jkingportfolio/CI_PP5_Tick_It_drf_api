23598edf54902b4b9f7d1633845114d9d7b6456f
jkingportfolio <mycodejourney3@gmail.com>
2023-04-25 16:53:30 +0000

    Add: Watch delete images to readme

--- tick_it_drf_api/settings.py
+++ tick_it_drf_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEV' in os.environ
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
